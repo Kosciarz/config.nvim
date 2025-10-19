e661d93a245e75a7acfe61f7028e111d7e411831
Bartosz Bachul <bartoszbachul1@gmail.com>
2025-10-19 21:44:09 +0200

    Update mason.nvim configuration to fix not working lsp servers

--- lua/config/plugins/mason.lua
+++ lua/config/plugins/mason.lua
@@ -1,55 +1,26 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {
-        ensure_installed = {
-            "lua_ls",
-            "ts_ls",
-            "pylsp",
-            "rust_analyzer",
-            "gopls",
-            "clangd",
-        },
-    },
-    dependencies = {
-        {
-            "folke/lazydev.nvim",
-            ft = "lua",
-            opts = {
-                library = {
-                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                },
-            },
-        },
-        {
-            "WhoIsSethDaniel/mason-tool-installer.nvim",
-        },
-        {
-            "mason-org/mason.nvim",
-            opts = {
-                ensure_installed = {
-                    "debugpy",
-                    "prettier",
-                    "black",
-                    "stylua",
-                    "cpplint",
-                    "cpptools",
-                },
-            },
-        },
-        "neovim/nvim-lspconfig",
-    },
-    config = function()
-        require("mason-tool-installer").setup({
-            ensure_installed = {
-                "debugpy",
-                "prettier",
-                "black",
-                "stylua",
-                "cpplint",
-                "cpptools",
-            },
-            auto_update = true,
-            run_on_start = true,
-        })
-    end,
+	"mason-org/mason-lspconfig.nvim",
+	opts = {
+		ensure_installed = {
+			"lua_ls",
+			"ts_ls",
+			"pylsp",
+			"rust_analyzer",
+			"gopls",
+			"clangd",
+		},
+	},
+	dependencies = {
+		{
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = {
+				library = {
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+        { "mason-org/mason.nvim", opts = {} },
+		"neovim/nvim-lspconfig",
+	},
 }
