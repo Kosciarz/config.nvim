65deeecf06dc23bc06e8f025d459f9d6b3982b05
Bartosz Bachul <bartoszbachul1@gmail.com>
2025-10-19 20:17:18 +0200

    Update lsp configuration

--- init.lua
+++ init.lua
@@ -3,7 +3,3 @@ require("config.lazy")
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
-vim.lsp.enable("lua_ls")
-vim.lsp.enable("pyright")
-vim.lsp.enable("clangd")
--- lua/config/plugins/mason.lua
+++ lua/config/plugins/mason.lua
@@ -1,25 +1,55 @@
 return {
-	"mason-org/mason-lspconfig.nvim",
-	opts = {
-		"lua_ls",
-		"ts_ls",
-		"pylsp",
-		"rust_analyzer",
-		"gopls",
-		"clangd",
-		"debugpy",
-	},
-	dependencies = {
-		{
-			"folke/lazydev.nvim",
-			ft = "lua",
-			opts = {
-				library = {
-					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-				},
-			},
-		},
-		{ "mason-org/mason.nvim", opts = {} },
-		"neovim/nvim-lspconfig",
-	},
+    "mason-org/mason-lspconfig.nvim",
+    opts = {
+        ensure_installed = {
+            "lua_ls",
+            "ts_ls",
+            "pylsp",
+            "rust_analyzer",
+            "gopls",
+            "clangd",
+        },
+    },
+    dependencies = {
+        {
+            "folke/lazydev.nvim",
+            ft = "lua",
+            opts = {
+                library = {
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
+        {
+            "WhoIsSethDaniel/mason-tool-installer.nvim",
+        },
+        {
+            "mason-org/mason.nvim",
+            opts = {
+                ensure_installed = {
+                    "debugpy",
+                    "prettier",
+                    "black",
+                    "stylua",
+                    "cpplint",
+                    "cpptools",
+                },
+            },
+        },
+        "neovim/nvim-lspconfig",
+    },
+    config = function()
+        require("mason-tool-installer").setup({
+            ensure_installed = {
+                "debugpy",
+                "prettier",
+                "black",
+                "stylua",
+                "cpplint",
+                "cpptools",
+            },
+            auto_update = true,
+            run_on_start = true,
+        })
+    end,
 }
